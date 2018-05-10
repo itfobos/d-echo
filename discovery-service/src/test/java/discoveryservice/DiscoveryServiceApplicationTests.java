c3120f21c6f30dd8a7180a33148ecca5d8b80012
ILya Turukin <tr.wolfer@gmail.com>
2018-05-10 23:53:46 +0700

    Added new 'discovery-service'. Intermediate commit.

---
+++ discovery-service/src/test/java/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,19 @@
+package discoveryservice;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@Ignore //TODO: Maybe make some integration test
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
