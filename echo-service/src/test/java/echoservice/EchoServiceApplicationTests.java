109431242d6a03863b22f686fde3c84252aa5ae4
ILYA Turukin <turukin@gmail.com>
2019-03-21 19:45:11 +0700

    [#16] Added Echo server.
    * Added shared config for Eureka, in Config server.
    * Added shared config for 'dev' profile.

---
+++ echo-service/src/test/java/echoservice/EchoServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package echoservice;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EchoServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
